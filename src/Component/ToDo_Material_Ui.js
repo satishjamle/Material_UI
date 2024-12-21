5a8d5b64a24b4482702e4659377671b0fd115e35
satishjamle <satishjamle513@gmail.com>
2024-12-21 22:32:53 +0530

    materialUi_ToDolist

--- src/Component/ToDo_Material_Ui.js
+++ src/Component/ToDo_Material_Ui.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
+import { ListComp } from "./ListComp";
 
 export const ToDo_Material_Ui = () => {
+  const [item, setitem] = useState("");
+  const [newitem, setNewItem] = useState([]);
+
+  const itemEvent = (event) => {
+    setitem(event.target.value);
+  };
+
+  const listOfitems = () => {
+    setNewItem((prevalue) => {
+      return [...prevalue, item];
+    });
+    setitem("");
+  };
+
   return (
     <>
-    <div className="main_div">
+      <div className="main_div">
         <div className="center_div">
-            <br />
-            <h1>ToDo List</h1>
-            <br />
-            <input type="text" placeholder='Add a item'/>
-<Button/>
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            value={item}
+            placeholder="Add a item"
+            onChange={itemEvent}
+          />
+          <Button className="newBtn" onClick={listOfitems}>
+            <AddSharpIcon />
+          </Button>
+          <br />
+          <ol>
+            {newitem.map((val,index) => {
+              return <ListComp key={index} text= {val} />;
+            })}
+          </ol>
         </div>
-    </div>
-
-    
+      </div>
     </>
-  )
-}
+  );
+};
